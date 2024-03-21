b43f1e15d850d38df94851af657a750cdd1fa3e2
MrOutlander <122881426+MrOutlander@users.noreply.github.com>
2024-03-21 22:30:41 +0000

    changed the bookmarks model to say users

--- mongodb/models/bookmarkedEvents.js
+++ mongodb/models/bookmarkedEvents.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const bookmarkedEventsSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
     event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true }
   });
   
